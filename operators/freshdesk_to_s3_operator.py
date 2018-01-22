dea0c4a490bc1cc2c854e817785099e3ddd51158
Adelina Enache <enache.adelina99@gmail.com>
2018-01-22 17:06:12 +0200

    Fix skipmixn

--- operators/freshdesk_to_s3_operator.py
+++ operators/freshdesk_to_s3_operator.py
@@ -149,8 +149,7 @@ class FreshdeskToS3Operator(BaseOperator, SkipMixin):
             # Write the results to a temporary file and save that file to s3.
             with NamedTemporaryFile("w") as tmp:
                 for result in results:
-                    filtered_result = self.filter_fields(result)
-                    tmp.write(json.dumps(filtered_result) + '\n')
+                    tmp.write(json.dumps(result) + '\n')
 
                 tmp.flush()
 
