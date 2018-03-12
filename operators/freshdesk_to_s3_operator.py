a837c29b52ad2b36666e1b8f31a29a3feb11c5f7
Benjamin Gregory <benjaminrgregory@gmail.com>
2018-03-12 12:26:24 -0600

    reorg imports; add templatable fields; reorg methods

--- operators/freshdesk_to_s3_operator.py
+++ operators/freshdesk_to_s3_operator.py
@@ -1,28 +1,13 @@
-from airflow.hooks.S3_hook import S3Hook
-from airflow.models import BaseOperator, SkipMixin
-
-from airflow.hooks.http_hook import HttpHook
-
-from dateutil.parser import parse
 from tempfile import NamedTemporaryFile
-
-import json
+from dateutil.parser import parse
 import logging
-import urllib
+import json
 
-mappings = {
-    'satisfaction_ratings': {
-        'endpoint': 'surveys/satisfaction_ratings'
-    },
-    'conversations': {
-        'parent': 'tickets',
-        'endpoint': 'tickets/{}/conversations'
-    },
-    'time_entries': {
-        'parent': 'tickets',
-        'endpoint': 'tickets/{}/time_entries'
-    }
-}
+from airflow.models import BaseOperator, SkipMixin
+from airflow.utils.decorators import apply_defaults
+
+from airflow.hooks.S3_hook import S3Hook
+from airflow.hooks.http_hook import HttpHook
 
 
 class FreshdeskToS3Operator(BaseOperator, SkipMixin):
@@ -32,11 +17,11 @@ class FreshdeskToS3Operator(BaseOperator, SkipMixin):
                                     credentials.
     :type freshdesk_conn_id:        string
     :param freshdesk_endpoint:      The endpoint to retrive data from.
-                                    Implemented for: 
+                                    Implemented for:
                                         - agents
                                         - companies
                                         - contacts
-                                        - conversations 
+                                        - conversations
                                         - groups
                                         - roles
                                         - satisfaction_ratings
@@ -56,6 +41,10 @@ class FreshdeskToS3Operator(BaseOperator, SkipMixin):
     :type updated_at:               string
     """
 
+    template_fields = ('s3_key',
+                       'updated_at')
+
+    @apply_defaults
     def __init__(self,
                  freshdesk_conn_id,
                  freshdesk_endpoint,
@@ -69,8 +58,7 @@ class FreshdeskToS3Operator(BaseOperator, SkipMixin):
 
         self.freshdesk_conn_id = freshdesk_conn_id
         self.freshdesk_endpoint = freshdesk_endpoint
-
-        self.updated_at = parse(updated_at) if updated_at else None
+        self.updated_at = updated_at
 
         self.s3_conn_id = s3_conn_id
         self.s3_bucket = s3_bucket
@@ -78,42 +66,29 @@ class FreshdeskToS3Operator(BaseOperator, SkipMixin):
 
         self.hook = None
 
-    def get_all(self, endpoint):
-        """
-        Get all pages.
-        """
-        response = self.hook.run(endpoint)
-        results = response.json()
-
-        while 'link' in response.headers:
-            response = self.hook.run(response.headers['link'])
-            results.extend(response.json())
-
-        # filter results
-        if self.updated_at:
-            results = [x for x in results
-                       if (not 'updated_at' in x) or
-                       (parse(x['updated_at']).replace(tzinfo=None) > self.updated_at)]
-
-        return results
-
     def execute(self, context):
         """
         Execute the operator.
         This will get all the data for a particular Freshdesk model
         and write it to a file.
         """
-        logging.info("Prepping to gather data from Freshdesk")
+        self.updated_at = parse(self.updated_at) if self.updated_at else None
 
-        self.hook = HttpHook(
-            method='GET',
-            http_conn_id=self.freshdesk_conn_id
-        )
+        self.hook = HttpHook(method='GET', http_conn_id=self.freshdesk_conn_id)
 
-        logging.info(
-            "Making request for"
-            " {0} object".format(self.freshdesk_endpoint)
-        )
+        mappings = {
+            'satisfaction_ratings': {
+                'endpoint': 'surveys/satisfaction_ratings'
+            },
+            'conversations': {
+                'parent': 'tickets',
+                'endpoint': 'tickets/{}/conversations'
+            },
+            'time_entries': {
+                'parent': 'tickets',
+                'endpoint': 'tickets/{}/time_entries'
+            }
+        }
 
         if self.freshdesk_endpoint in mappings:
             if 'parent' in mappings[self.freshdesk_endpoint]:
@@ -134,8 +109,7 @@ class FreshdeskToS3Operator(BaseOperator, SkipMixin):
 
         if len(results) == 0 or results is None:
             logging.info("No records pulled from Freshdesk.")
-            downstream_tasks = context['task'].get_flat_relatives(
-                upstream=False)
+            downstream_tasks = context['task'].get_flat_relatives(upstream=False)
             logging.info('Skipping downstream tasks...')
             logging.debug("Downstream task_ids %s", downstream_tasks)
 
@@ -163,3 +137,22 @@ class FreshdeskToS3Operator(BaseOperator, SkipMixin):
                 )
                 dest_s3.connection.close()
                 tmp.close()
+
+    def get_all(self, endpoint):
+        """
+        Get all pages.
+        """
+        response = self.hook.run(endpoint)
+        results = response.json()
+
+        while 'link' in response.headers:
+            response = self.hook.run(response.headers['link'])
+            results.extend(response.json())
+
+        # filter results
+        if self.updated_at:
+            results = [x for x in results
+                       if ('updated_at' not in x) or
+                       (parse(x['updated_at']).replace(tzinfo=None) > self.updated_at)]
+
+        return results
